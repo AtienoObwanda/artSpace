90dd83eb9ff0753309f364a19a70657ffb2dad87
atienoobwanda <atienoobwanda@gmail.com>
2022-05-27 22:32:19 +0300

    Adding the application urls a nd their views for set up

---
+++ space/urls.py
@@ -0,0 +1,10 @@
+from django import views
+from django.urls import path
+from .import views
+urlpatterns = [
+    # path('', admin.site.urls),
+    path('', views.home, name='home'),
+    path('/explore', views.explore, name='explore'),
+    path('/search', views.search, name='search')
+
+]
--- space/views.py
+++ space/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 # Create your views here.
+
+# View for home page
+def home(request):
+    return HttpResponse('Home')
+
+# view for explore page
+def explore(request):
+    return HttpResponse('Explore')
+# view for search page
+def search(request):
+    return HttpResponse('Search')
