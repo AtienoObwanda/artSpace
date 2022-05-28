1de7fafbda86de1c7e010101768ad685680defd2
atienoobwanda <atienoobwanda@gmail.com>
2022-05-28 11:49:12 +0300

    Initial migration

--- Art/settings.py
+++ Art/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = 'static/'
 #     os.path.join(BASE_DIR, 'static'),
 # )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
