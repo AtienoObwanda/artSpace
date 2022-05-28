d1012b42496a7a97c3573f88c4bfe1b550260f60
atienoobwanda <atienoobwanda@gmail.com>
2022-05-28 11:01:58 +0300

    Creating the models(images, tags,categories,location) etc

--- space/models.py
+++ space/models.py
@@ -1,4 +1,40 @@
+from unicodedata import category
 from django.db import models
-
+from 
 # Create your models here.
+# Models used are: image,category,location, and user
 
+# class User(models.Model):
+#     firstName = models.CharField(max_length=30)
+#     lastName = models.CharField(max_length=30)
+#     email = models.EmailField()
+#     phone_number = models.CharField(max_length = 10,blank =True)
+
+# Category model
+class Category(models.Model):
+    name = models.CharField()
+    def __str__(self):
+        return self.name
+
+# Location model
+class Location(models.Model):
+    name = models.CharField()
+    def __str__(self):
+        return self.name
+
+# Tags model
+class Tags(models.Model):
+    name = models.CharField()
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'articles/', blank=True)
+    imageName = models.CharField(max_length=60)
+    description = models.TextField()
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    tags= models.ManyToManyField(Tags)
+
+    def __str__(self):
+        return self.imageName
