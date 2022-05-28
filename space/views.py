b7bfd8f4912b044a3f8e3096329b1fcd3eba42df
atienoobwanda <atienoobwanda@gmail.com>
2022-05-28 12:19:37 +0300

    updating views to fetch the images

--- space/views.py
+++ space/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Image
 # Create your views here.
 
 # View for home page
@@ -7,6 +8,8 @@ def home(request):
     return render (request, 'art/home.html')
 # view for explore page
 def explore(request):
-    return render (request, 'art/explore.html')# view for search page
+    images = Image
+
+    return render (request, 'art/explore.html', {"images":images})# view for search page
 def search(request):
     return render (request, 'art/search.html')
