d6eee9cdac7f1005448f2143deae0571d141055e
atienoobwanda <atienoobwanda@gmail.com>
2022-05-28 11:32:09 +0300

    Updating images filed to use the ResizedImageField from Django

--- space/models.py
+++ space/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django_resized import ResizedImageField
+from django.utils import timezone
+from flask_sqlalchemy import Model
 
+# from django.conf import settings
 
 # Models created are: image,category,location, and user
 
@@ -28,10 +31,13 @@ class Tags(models.Model):
     def __str__(self):
         return self.name
 
+# Images Model
 class Image(models.Model):
-    image = models.ImageField(upload_to = 'articles/', blank=True)
+    # image = models.ImageField(upload_to = 'articles/', blank=True)
+    image = ResizedImageField(size=[500, 500], crop=['middle', 'center'], upload_to='articles/', blank=True)
     imageName = models.CharField(max_length=60)
     description = models.TextField()
+
     location = models.ForeignKey(Location,on_delete=models.CASCADE)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     tags= models.ManyToManyField(Tags)
