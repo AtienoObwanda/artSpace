3e219da9e06a807bb509a52716932a5c9318116a
atienoobwanda <atienoobwanda@gmail.com>
2022-05-28 13:59:21 +0300

    Running tests to fail: Running 4 tests but expecting failer since the model for location and category has no save() method

--- space/tests.py
+++ space/tests.py
@@ -1,11 +1,37 @@
 from django.test import TestCase
 from .models import *
 # Create your tests here.
+
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.kenya = Location(name='Kenya')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.kenya,Location)) 
+
+    def test_save_method(self):
+        self.kenya.save_category()
+        location = Location.objects.all()
+        self.assertTrue(len(location)>0)   
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.food = Category(name='Food')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.food,Category)) 
+
+    def test_save_method(self):
+        self.food.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories)>0)   
+
 # Image Test
 class ImageTest(TestCase):
     def setUp(self):
-        self.new_category = Category(name='Food')
-        self.new_category.save()
+        self.Food = Category(name='Food')
+        self.Food.save_category()
 
         self.new_location = Location(name='Kenya')
         self.new_location.save()
@@ -13,15 +39,21 @@ class ImageTest(TestCase):
         self.new_tag = Tags(name = 'testing')
         self.new_tag.save()
 
-        self.new_image=Image(image_name='food',image_description='Chinese Delicacy')
+        self.new_image=Image(image_name='foods',image_description='Chinese Delicacy')
         self.new_image.save_image() 
 
         self.new_image.Category.add(self.new_category)
         self.new_image.Location.add(self.new_location)
         self.new_image.Tags.add(self.new_tag)
 
-        # def tearDown(self):
-        # Image.objects.all().delete()
-        # tags.objects.all().delete()
-        # Article.objects.all().delete()
+        def tearDown(self):
+            Image.objects.all().delete()
+            Category.objects.all().delete()
+            Location.objects.all().delete()
+            Tags.objects.all().delete()
+
+        def test_getImages(self):
+            allImages = Image.getImages()
+            self.assertTrue(len(allImages)>0)
+
 
